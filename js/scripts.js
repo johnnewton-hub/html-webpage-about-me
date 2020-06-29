3fb78d91509936b2d9ae3a87162bca48d2ce7818
James Grieve <jgrieve@ualberta.ca>
2020-06-29 15:42:45 -0600

    Added a JavaScript example for people to fiddle with;

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-console.log( "Hello, World!" );
+// prompt(); = Console.ReadLine(); in C#
+let userInput = prompt("Please enter a number to be cubed: ");
+
+// console.log(); = Console.WriteLine(); in C#
+console.log(userInput ** 3);
