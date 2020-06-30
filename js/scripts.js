195a3a003db25cd7b699d60d359a70b3dc33b0b5
James Grieve <jgrieve@ualberta.ca>
2020-06-30 09:33:24 -0600

    Commented out JS demo;

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
+/* Cubing of Numbers
+
 // prompt(); = Console.ReadLine(); in C#
 let userInput = prompt("Please enter a number to be cubed: ");
 
 // console.log(); = Console.WriteLine(); in C#
 console.log(userInput ** 3);
+
+*/
